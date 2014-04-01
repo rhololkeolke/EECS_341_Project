fadbf21077413567c3a6bff96bf298033144e94d
Devin Schwab <digidevin@gmail.com>
2014-03-31 22:57:12 -0400

    Created basic product scraper. Seems to grab all of the relevant product information properly.

--- microcenter_scraper/microcenter_scraper/spiders/product_spider.py
+++ microcenter_scraper/microcenter_scraper/spiders/product_spider.py
@@ -3,6 +3,8 @@ from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.selector import Selector
 from scrapy.http import Request
 
+from string import strip
+
 from microcenter_scraper.items import Product
 
 class MicrocenterProductSpider(CrawlSpider):
@@ -11,10 +13,23 @@ class MicrocenterProductSpider(CrawlSpider):
     start_urls = [
         "http://www.microcenter.com"
     ]
-    rules = [Rule(SgmlLinkExtractor(allow=['/category/[\d]+,?[\d]*/[a-zA-Z]+']), callback='parse_page')]
+    rules = [
+        Rule(SgmlLinkExtractor(allow=['/category/[\d]+,?[\d]*/[a-zA-Z]+'])),
+        Rule(SgmlLinkExtractor(allow=['/product/[\d]+/.*']), callback='parse_product')
+    ]
 
-    def parse_page(self, response):
+    def parse_product(self, response):
         sel = Selector(response)
-        title = sel.xpath('//title/text()').extract()
-        print title
-            
+        product = Product()
+        product['name'] = map(strip, sel.xpath("//div[@itemprop='name']/text()").extract())
+        product['desc'] = map(strip, sel.xpath("//div[@itemprop='description']//*[not(self::script)]//text()").extract())
+        price_values = map(strip, sel.xpath("//span[@itemprop='price']/text()").extract() + \
+                       sel.xpath("//span[@itemprop='price']/span[last()]/text()").extract())
+        product['price'] = ['.'.join(price_values)]
+        
+        category_link = sel.xpath('//div[@id="product-details-control"]/h1/small/a[position()>1]/text()').extract()
+        product['category'] = category_link
+        print "category_link", category_link
+        product['brand'] = map(strip, sel.xpath('//small[@itemprop="brand"]/a/text()').extract())
+        product['upc'] = map(strip, sel.xpath('//div[@id="detail-list"]/dl/dd[last()]/text()').extract())
+        return product
