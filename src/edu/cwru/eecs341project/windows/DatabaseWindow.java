6bf6347d9c8f3819d99f68436742205cbf2e8b87
Devin Schwab <digidevin@gmail.com>
2014-04-09 19:36:31 -0400

    Now the table schema part works.

--- src/edu/cwru/eecs341project/windows/DatabaseWindow.java
+++ src/edu/cwru/eecs341project/windows/DatabaseWindow.java
@@ -2,6 +2,7 @@ package edu.cwru.eecs341project.windows;
 
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
@@ -19,6 +20,7 @@ import com.googlecode.lanterna.gui.component.Panel;
 import com.googlecode.lanterna.gui.component.TextArea;
 import com.googlecode.lanterna.gui.dialog.ListSelectDialog;
 import com.googlecode.lanterna.gui.dialog.MessageBox;
+import com.googlecode.lanterna.gui.dialog.TextInputDialog;
 
 import edu.cwru.eecs341project.GlobalState;
 import edu.cwru.eecs341project.WindowManager;
@@ -144,6 +146,87 @@ public class DatabaseWindow extends MicrocenterWindow {
 		}
 	}
 	
+	private class TableSchemaWindow extends MicrocenterWindow {
+		
+		public TableSchemaWindow(GUIScreen guiScreen, String tableName)
+		{
+			super(guiScreen, tableName + " Schema", true, false);
+			
+			String result = "";
+			Connection conn = GlobalState.getDBConnection();
+			try {
+				// assuming the DBA is trusted so I'm not using a prepared statement
+				Statement schemaQuery = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
+				ResultSet rs = schemaQuery.executeQuery("SELECT * FROM " + tableName + " LIMIT 0;");
+				result = resultsToString(rs);
+				rs.close();
+				schemaQuery.close();
+			} catch(SQLException e) {
+				result = "ERROR: " + e.getMessage();
+			}
+			
+			Panel resultsPanel = new Panel();
+			resultsPanel.addComponent(new TextArea(guiScreen.getScreen().getTerminalSize(), result));
+			addComponent(resultsPanel);
+			
+		}
+		
+		private String resultsToString(ResultSet rs) throws SQLException {
+			ResultSetMetaData rsmd = rs.getMetaData();
+			String[] headers = {"Name", "Type"};
+			int[] maxColLength = new int[2];
+			
+			for(int i=0; i<headers.length; i++)
+			{
+				maxColLength[0] = headers[i].length();
+				maxColLength[1] = headers[i].length();
+			}
+			
+			for(int i=1; i<=rsmd.getColumnCount(); i++)
+			{
+				maxColLength[0] = Math.max(maxColLength[0], rsmd.getColumnLabel(i).length());
+				maxColLength[1] = Math.max(maxColLength[1], rsmd.getColumnTypeName(i).length());
+			}
+			
+			StringBuilder sb = new StringBuilder();
+			sb.append("|");
+			for(int i=0; i<headers.length; i++)
+			{
+				sb.append(" " + headers[i]);
+				for(int j=0; j<maxColLength[i] - headers[i].length(); j++)
+				{
+					sb.append(" ");
+				}
+				sb.append(" |");
+			}
+			sb.append("\n-");
+			for(int i=0; i<maxColLength.length; i++)
+			{
+				for(int j=0; j<maxColLength[i] + 3; j++)
+					sb.append("-");
+			}
+			sb.append("\n");
+			
+			for(int i=1; i<=rsmd.getColumnCount(); i++)
+			{
+				sb.append("| " + rsmd.getColumnLabel(i));
+				for(int j=0; j<maxColLength[0] - rsmd.getColumnLabel(i).length(); j++)
+				{
+					sb.append(" ");
+				}
+				sb.append(" |");
+				sb.append(" " + rsmd.getColumnTypeName(i));
+				for(int j=0; j<maxColLength[1] - rsmd.getColumnTypeName(i).length(); j++)
+				{
+					sb.append(" ");
+				}
+				sb.append(" |\n");
+			}
+			
+			return sb.toString();
+		}
+	}
+	
 	private class ActionListBoxItem implements Action {
         private String label;
 
@@ -157,6 +240,7 @@ public class DatabaseWindow extends MicrocenterWindow {
         }
 
         public void doAction() {
+        	String selectedTable = null;
         	if(label.equals("Raw Query")) {
         		//TextInputDialog.showTextInputBox(guiScreen, "SQL Query", "Enter query to execute", "SELECT * FROM customer LIMIT 10");
         		guiScreen.showWindow(new RawQueryWindow(guiScreen), GUIScreen.Position.FULL_SCREEN);
@@ -164,11 +248,9 @@ public class DatabaseWindow extends MicrocenterWindow {
         		
         		List<String> tableNames = new ArrayList<String>();
         		Connection dbConnection = GlobalState.getDBConnection();
-        		DatabaseMetaData md = null;
-        		ResultSet rs = null;
         		try {
-        			md = dbConnection.getMetaData();
-        			rs = md.getTables(null, null, "%", null);
+        			DatabaseMetaData md = dbConnection.getMetaData();
+        			ResultSet rs = md.getTables(null, "public", "%" ,new String[] {"TABLE"} );
         			while(rs.next()) {
         				tableNames.add(rs.getString(3));
         			}
@@ -177,10 +259,13 @@ public class DatabaseWindow extends MicrocenterWindow {
         			return;
         		}
         		
-        		String selectedTable = (String)ListSelectDialog.showDialog(guiScreen, "Table Names", "List of tables in the database", tableNames.toArray());
-        		System.out.println(selectedTable);
-        	} else if(label.equals("Show Table Schmea")) {
-        		
+        		selectedTable = (String)ListSelectDialog.showDialog(guiScreen, "Table Names", "List of tables in the database", tableNames.toArray());
+        	} else if(label.equals("Show Table Schema")) {
+        		selectedTable = TextInputDialog.showTextInputBox(guiScreen, "Table Name", "Enter a table name to get its schema", "");
+        	}
+        	
+        	if(selectedTable != null) {
+        		guiScreen.showWindow(new TableSchemaWindow(guiScreen, selectedTable), GUIScreen.Position.FULL_SCREEN);
         	}
         }
 	}
