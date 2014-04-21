b140805895fe85cd77ddc2211c62a1ce8a2955ba
Devin Schwab <digidevin@gmail.com>
2014-04-21 04:03:27 -0400

    List Customers now working

--- src/edu/cwru/eecs341project/windows/CustomersWindow.java
+++ src/edu/cwru/eecs341project/windows/CustomersWindow.java
@@ -1,10 +1,22 @@
 package edu.cwru.eecs341project.windows;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import com.googlecode.lanterna.gui.Action;
 import com.googlecode.lanterna.gui.GUIScreen;
 import com.googlecode.lanterna.gui.component.ActionListBox;
 import com.googlecode.lanterna.gui.component.Panel;
+import com.googlecode.lanterna.gui.dialog.ListSelectDialog;
 import com.googlecode.lanterna.gui.dialog.MessageBox;
+
+import edu.cwru.eecs341project.GlobalState;
 
 public class CustomersWindow extends MicrocenterWindow {
 	private Panel actionsPanel;
@@ -15,6 +27,7 @@ public class CustomersWindow extends MicrocenterWindow {
 		actionsPanel = new Panel();
 		ActionListBox actionListBox = new ActionListBox();
 		actionListBox.addAction(new ActionListBoxItem("Search"));
+		actionListBox.addAction(new ActionListBoxItem("List Customers"));
 		actionListBox.addAction(new ActionListBoxItem("Customer Info"));
 		actionsPanel.addComponent(actionListBox);
         addComponent(actionsPanel);
@@ -33,7 +46,41 @@ public class CustomersWindow extends MicrocenterWindow {
         }
 
         public void doAction() {
-            MessageBox.showMessageBox(guiScreen, "Action", "Selected " + label);
+        	if(label.equals("List Customers"))
+        	{
+        		Connection dbConn = GlobalState.getDBConnection();
+        		List<String> listOptions = new ArrayList<String>();
+        		Map<String, Integer> nameToLoyaltyMap = new HashMap<String, Integer>();
+        		try {
+        			Statement st = dbConn.createStatement();
+        			ResultSet rs = st.executeQuery("SELECT loyalty_number, last_name, first_name, middle_initial FROM customer ORDER BY last_name, first_name, middle_initial;");
+        			while(rs.next())
+        			{
+        				StringBuilder nameBuilder = new StringBuilder();
+        				nameBuilder.append(rs.getString(2));
+        				nameBuilder.append(", ");
+        				nameBuilder.append(rs.getString(3));
+        				nameBuilder.append(" ");
+        				String middleInitial = rs.getString(4);
+        				if(middleInitial != null)
+        				{
+        					nameBuilder.append(middleInitial);
+        					nameBuilder.append(".");
+        				}
+        				
+        				nameToLoyaltyMap.put(nameBuilder.toString(), rs.getInt(1));
+        				listOptions.add("[" + rs.getInt(1) + "]" + nameBuilder.toString());
+        			}
+        			
+        			ListSelectDialog.showDialog(guiScreen, "Customer List", "All customers in database", listOptions.toArray());
+        		} catch(SQLException e) {
+        			MessageBox.showMessageBox(guiScreen, "SQL Error", "Error getting list of customers" + e.getMessage());
+        			return;
+        		}
+        	}
+        	else {
+        		MessageBox.showMessageBox(guiScreen, "Action", "Selected " + label);
+        	}
         }
 	}
 }
