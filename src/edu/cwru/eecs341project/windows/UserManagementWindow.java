5f119b045ff375515edd6fbf9069c993aa7466e9
Devin Schwab <digidevin@gmail.com>
2014-04-21 03:13:01 -0400

    Showing a list of all customers

--- src/edu/cwru/eecs341project/windows/UserManagementWindow.java
+++ src/edu/cwru/eecs341project/windows/UserManagementWindow.java
@@ -1,10 +1,22 @@
 package edu.cwru.eecs341project.windows;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import com.googlecode.lanterna.gui.Action;
 import com.googlecode.lanterna.gui.GUIScreen;
 import com.googlecode.lanterna.gui.component.ActionListBox;
 import com.googlecode.lanterna.gui.component.Panel;
+import com.googlecode.lanterna.gui.dialog.ListSelectDialog;
 import com.googlecode.lanterna.gui.dialog.MessageBox;
+
+import edu.cwru.eecs341project.GlobalState;
 
 public class UserManagementWindow extends MicrocenterWindow {
 	
@@ -37,7 +49,41 @@ public class UserManagementWindow extends MicrocenterWindow {
         }
 
         public void doAction() {
-            MessageBox.showMessageBox(owner, "Action", "Selected " + label);
+        	if(label.equals("List Users"))
+        	{
+        		Connection dbConn = GlobalState.getDBConnection();
+        		List<String> listOptions = new ArrayList<String>();
+        		Map<String, Integer> nameToLoyaltyMap = new HashMap<String, Integer>();
+        		try {
+        			Statement st = dbConn.createStatement();
+        			ResultSet rs = st.executeQuery("SELECT loyalty_number, last_name, first_name, middle_initial FROM customer ORDER BY last_name, first_name, middle_initial;");
+        			while(rs.next())
+        			{
+        				StringBuilder nameBuilder = new StringBuilder();
+        				nameBuilder.append(rs.getString(2));
+        				nameBuilder.append(", ");
+        				nameBuilder.append(rs.getString(3));
+        				nameBuilder.append(" ");
+        				String middleInitial = rs.getString(4);
+        				if(middleInitial != null)
+        				{
+        					nameBuilder.append(middleInitial);
+        					nameBuilder.append(".");
+        				}
+        				
+        				nameToLoyaltyMap.put(nameBuilder.toString(), rs.getInt(1));
+        				listOptions.add("[" + rs.getInt(1) + "]" + nameBuilder.toString());
+        			}
+        			
+        			ListSelectDialog.showDialog(guiScreen, "Customer List", "All customers in database", listOptions.toArray());
+        		} catch(SQLException e) {
+        			MessageBox.showMessageBox(guiScreen, "SQL Error", "Error getting list of customers" + e.getMessage());
+        			return;
+        		}
+        	}
+        	else {
+        		MessageBox.showMessageBox(owner, "Action", "Selected " + label);
+        	}
         }
 	}
 
