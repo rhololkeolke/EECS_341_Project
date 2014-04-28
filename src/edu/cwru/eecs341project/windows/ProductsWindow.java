a6531e02d16cdf2eaac594ebfe5ff944f9a213e0
Devin Schwab <digidevin@gmail.com>
2014-04-27 23:06:17 -0400

    All of the static information is being displayed

--- src/edu/cwru/eecs341project/windows/ProductsWindow.java
+++ src/edu/cwru/eecs341project/windows/ProductsWindow.java
@@ -12,12 +12,17 @@ import java.util.Map;
 import com.googlecode.lanterna.gui.Action;
 import com.googlecode.lanterna.gui.GUIScreen;
 import com.googlecode.lanterna.gui.component.ActionListBox;
+import com.googlecode.lanterna.gui.component.Button;
+import com.googlecode.lanterna.gui.component.Label;
 import com.googlecode.lanterna.gui.component.Panel;
+import com.googlecode.lanterna.gui.component.TextArea;
+import com.googlecode.lanterna.gui.component.TextBox;
 import com.googlecode.lanterna.gui.dialog.ListSelectDialog;
 import com.googlecode.lanterna.gui.dialog.MessageBox;
 import com.googlecode.lanterna.gui.dialog.TextInputDialog;
 
 import edu.cwru.eecs341project.GlobalState;
+import edu.cwru.eecs341project.WindowManager;
 
 public class ProductsWindow extends MicrocenterWindow {
 	
@@ -104,15 +109,218 @@ public class ProductsWindow extends MicrocenterWindow {
         				return;
         			}
         			String selectedProduct = (String)ListSelectDialog.showDialog(guiScreen, "Search Results", "Found " + searchResults.size() + " matches", searchResults.toArray());
-        			System.out.println(searchResultsMap.get(selectedProduct));
+        			if(selectedProduct == null)
+        				return; // user canceled
+        			ProductInfoWindow window = new ProductInfoWindow(guiScreen, searchResultsMap.get(selectedProduct));
+        			WindowManager.pushWindow(window);
+        			guiScreen.showWindow(window, GUIScreen.Position.FULL_SCREEN);
         		} catch(SQLException e) {
         			MessageBox.showMessageBox(guiScreen, "SQL Error", e.getMessage());
         			System.out.println(e.getMessage());
         		}
+        	} else if(label.equals("Product Info")) {
+        		String upcString = TextInputDialog.showTextInputBox(guiScreen, "Product UPC", "Enter the UPC of the product you want to see informaiton about", "");
+        		if(upcString == null)
+        			return; // user canceled
+        		ProductInfoWindow window = new ProductInfoWindow(guiScreen, Long.parseLong(upcString));
+        		WindowManager.pushWindow(window);
+        		guiScreen.showWindow(window, GUIScreen.Position.FULL_SCREEN);
         	} else {
         		MessageBox.showMessageBox(guiScreen, "Action", "Selected " + label);
         	}
         }
+	}
+	
+	private class ProductInfoWindow extends MicrocenterWindow
+	{
+		private final Long upc;
+		private String name;
+		private String desc;
+		private double unitPrice;
+		private String brand;
+		private List<String> categories;
+		private int currStore;
+		private String currStoreName = "Website";
+		private int stock;
+		
+		private Panel mainPanel;
+		private TextBox  nameBox;
+		private TextArea descArea;
+		private TextBox unitPriceBox;
+		private Button brandButton;
+		private Button storeButton;
+		public ProductInfoWindow(final GUIScreen guiScreen, Long upc)
+		{
+			super(guiScreen, "Product Info", true, false);
+			this.upc = upc;
+			
+			Connection dbConn = GlobalState.getDBConnection();
+			try {
+				PreparedStatement st = dbConn.prepareStatement("SELECT p.name, p.desc, p.unit_price, b.name " +
+						"FROM product as p, " +
+						"     brand as b " +
+						"WHERE p.brand = b.id AND " +
+						"      p.upc = ?;");
+				st.setLong(1, upc);
+				ResultSet rs = st.executeQuery();
+				if(!rs.next())
+				{
+					MessageBox.showMessageBox(guiScreen, "Error", "Could not find product " + upc);
+					close();
+					return;
+				}
+				name = rs.getString(1);
+				desc = rs.getString(2);
+				unitPrice = rs.getDouble(3);
+				brand = rs.getString(4);
+				
+				rs.close();
+				st.close();
+				
+				// get the category hierarchy
+				st = dbConn.prepareStatement("SELECT parent.name " +
+						"FROM product_type_tree as node, " +
+						"     product_type_tree as parent, " +
+						"     product_type as pt " +
+						"WHERE pt.id = node.id AND " +
+						"      pt.upc = ? AND " +
+						"      node.lft BETWEEN parent.lft AND parent.rgt " +
+						"ORDER BY parent.lft;");
+				st.setLong(1, upc);
+				rs = st.executeQuery();
+				
+				categories = new ArrayList<String>();
+				while(rs.next())
+				{
+					categories.add(rs.getString(1));
+				}
+				
+				rs.close();
+				st.close();
+				
+				// get the stock information for this product
+				st = dbConn.prepareStatement("SELECT s.id, st.amount " +
+						"FROM stock st, " +
+						"     store as s " +
+						"WHERE st.store_id = s.id AND " +
+						"      s.name = ? AND " +
+						"      st.upc = ?;");
+				st.setString(1, currStoreName);
+				st.setLong(2, upc);
+				rs = st.executeQuery();
+				
+				if(!rs.next())
+				{
+					MessageBox.showMessageBox(guiScreen, "Error", "Could not get stock information");
+					close();
+					return;
+				}
+				
+				currStore = rs.getInt(1);
+				stock = rs.getInt(2);
+				
+				rs.close();
+				st.close();
+			} catch(SQLException e) {
+				MessageBox.showMessageBox(guiScreen, "SQL Error", e.getMessage());
+				System.out.println(e.getMessage());
+			}
+			
+			mainPanel = new Panel();
+			
+			Panel categoryPanel = new Panel(Panel.Orientation.HORISONTAL);
+			for(String cat : categories)
+			{
+				categoryPanel.addComponent(new Button(cat, new Action() {
+					@Override
+					public void doAction() {
+						MessageBox.showMessageBox(guiScreen, "TODO", "TODO");
+					}
+				}));
+			}
+			mainPanel.addComponent(categoryPanel);
+			
+			Panel upcPanel = new Panel(Panel.Orientation.HORISONTAL);
+			upcPanel.addComponent(new Label("UPC: "));
+			upcPanel.addComponent(new Label(""+upc));
+			mainPanel.addComponent(upcPanel);
+			
+			Panel namePanel = new Panel(Panel.Orientation.HORISONTAL);
+			namePanel.addComponent(new Label("Name: "));
+			if(GlobalState.getUserRole() == GlobalState.UserRole.DBA || GlobalState.getUserRole() == GlobalState.UserRole.EMPLOYEE)
+			{
+				nameBox = new TextBox(name);
+				namePanel.addComponent(nameBox);
+			}
+			else
+			{
+				namePanel.addComponent(new Label(name));
+			}
+			mainPanel.addComponent(namePanel);
+			
+			Panel brandPanel = new Panel(Panel.Orientation.HORISONTAL);
+			brandPanel.addComponent(new Label("Brand: "));
+			if(GlobalState.getUserRole() == GlobalState.UserRole.DBA || GlobalState.getUserRole() == GlobalState.UserRole.EMPLOYEE)
+			{
+				brandButton = new Button(brand, new Action() {
+					@Override
+					public void doAction() {
+						MessageBox.showMessageBox(guiScreen, "TODO", "TODO");
+					}
+				});
+				brandPanel.addComponent(brandButton);
+			}
+			else
+			{
+				brandPanel.addComponent(new Label(brand));
+			}
+			mainPanel.addComponent(brandPanel);
+			
+			Panel descPanel = new Panel(Panel.Orientation.HORISONTAL);
+			descPanel.addComponent(new Label("Description: "));
+			String[] descStrings = desc.split("[.!]");
+			StringBuilder newlineDesc = new StringBuilder();
+			for(String line : descStrings)
+			{
+				newlineDesc.append(line);
+				newlineDesc.append("\n");
+			}
+			descArea = new TextArea(newlineDesc.toString());
+			descPanel.addComponent(descArea);
+			mainPanel.addComponent(descPanel);
+			
+			Panel pricePanel = new Panel(Panel.Orientation.HORISONTAL);
+			pricePanel.addComponent(new Label("Price: $"));
+			if(GlobalState.getUserRole() == GlobalState.UserRole.DBA || GlobalState.getUserRole() == GlobalState.UserRole.EMPLOYEE)
+			{
+				unitPriceBox = new TextBox(""+unitPrice);
+				pricePanel.addComponent(unitPriceBox);
+			}
+			else
+			{
+				pricePanel.addComponent(new Label(""+unitPrice));
+			}
+			mainPanel.addComponent(pricePanel);
+
+			mainPanel.addComponent(new Button("View Specs"));
+			
+			Panel stockPanel = new Panel(Panel.Orientation.HORISONTAL);
+			storeButton = new Button(currStoreName);
+			stockPanel.addComponent(storeButton);
+			stockPanel.addComponent(new Label(" stock: "));
+			stockPanel.addComponent(new Label("" + stock));
+			mainPanel.addComponent(stockPanel);
+			
+			Panel buttonsPanel = new Panel(Panel.Orientation.HORISONTAL);
+			Button addToCartButton = new Button("Add to Cart");
+			buttonsPanel.addComponent(addToCartButton);
+			Button saveButton = new Button("Save Changes");
+			buttonsPanel.addComponent(saveButton);
+			mainPanel.addComponent(buttonsPanel);
+			
+			addComponent(mainPanel);
+		}
+		
 	}
 
 }
