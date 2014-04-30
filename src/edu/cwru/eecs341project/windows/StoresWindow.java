3ce7d9cfb1663e6c82ca20f3556b9ac84ab93f71
Devin Schwab <digidevin@gmail.com>
2014-04-30 07:33:35 -0400

    Can now search for and display stores

--- src/edu/cwru/eecs341project/windows/StoresWindow.java
+++ src/edu/cwru/eecs341project/windows/StoresWindow.java
@@ -1,10 +1,26 @@
 package edu.cwru.eecs341project.windows;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import com.googlecode.lanterna.gui.Action;
 import com.googlecode.lanterna.gui.GUIScreen;
 import com.googlecode.lanterna.gui.component.ActionListBox;
+import com.googlecode.lanterna.gui.component.Button;
 import com.googlecode.lanterna.gui.component.Panel;
+import com.googlecode.lanterna.gui.dialog.ListSelectDialog;
 import com.googlecode.lanterna.gui.dialog.MessageBox;
+import com.googlecode.lanterna.gui.dialog.TextInputDialog;
+
+import edu.cwru.eecs341project.GlobalState;
+import edu.cwru.eecs341project.WindowManager;
 
 public class StoresWindow extends MicrocenterWindow {
 	private Panel actionsPanel;
@@ -34,7 +50,85 @@ public class StoresWindow extends MicrocenterWindow {
         }
 
         public void doAction() {
-            MessageBox.showMessageBox(guiScreen, "Action", "Selected " + label);
+        	if(label.equals("List Stores"))
+        	{
+        		Connection dbConn = GlobalState.getDBConnection();
+        		try {
+        			Statement st = dbConn.createStatement();
+	        		ResultSet rs = st.executeQuery(
+	        					"SELECT id, name " +
+	        					"FROM store;");
+	        		
+	        		List<String> options = new ArrayList<String>();
+	        		Map<String, Integer> storeIds = new HashMap<String, Integer>();
+	        		
+	        		while(rs.next())
+	        		{
+	        			options.add("["+rs.getInt(1)+"]" + rs.getString(2));
+	        			storeIds.put("["+rs.getInt(1)+"]"+rs.getString(2), rs.getInt(1));
+	        		}
+	        		
+	        		String selected = (String)ListSelectDialog.showDialog(guiScreen, "Stores", "Select a store", options.toArray());
+	        		if(selected == null)
+	        			return; // user canceled
+	        		StoreInfoWindow window = new StoreInfoWindow(guiScreen, storeIds.get(selected));
+	        		WindowManager.pushWindow(window);
+	        		guiScreen.showWindow(window, GUIScreen.Position.FULL_SCREEN);
+	        		rs.close();
+	        		st.close();
+        		} catch(SQLException e) {
+        			MessageBox.showMessageBox(guiScreen, "SQL Error", e.getMessage());
+        		}
+        	}
+        	else if(label.equals("Search Stores")) {
+        		Connection dbConn = GlobalState.getDBConnection();
+        		   		
+        		try {
+		        	String searchString = TextInputDialog.showTextInputBox(guiScreen, "Store Search", "Enter a state abbreviation (e.g. OH)", "");
+		        			
+		        			
+	        		String state = searchString;
+	        		PreparedStatement st = dbConn.prepareStatement(
+	        						"SELECT id, name " +
+	        						"FROM store " +
+	        						"WHERE LOWER(state) LIKE LOWER(?);");
+	        		st.setString(1, state);
+		        	ResultSet rs = st.executeQuery();
+	
+		        	List<String> options = new ArrayList<String>();
+		        	Map<String, Integer> storeIds = new HashMap<String, Integer>();
+		        		
+		        	while(rs.next())
+		        	{
+		        		options.add("["+rs.getInt(1)+"]" + rs.getString(2));
+		        		storeIds.put("["+rs.getInt(1)+"]"+rs.getString(2), rs.getInt(1));
+		        	}
+		        		
+		        	String selected = (String)ListSelectDialog.showDialog(guiScreen, "Stores", "Select a store", options.toArray());
+		        	if(selected == null)
+		        		return; // user canceled
+		        	StoreInfoWindow window = new StoreInfoWindow(guiScreen, storeIds.get(selected));
+		        	WindowManager.pushWindow(window);
+		        	guiScreen.showWindow(window, GUIScreen.Position.FULL_SCREEN);
+		        	rs.close();
+		        	st.close();
+        		} catch(SQLException e) {
+        			MessageBox.showMessageBox(guiScreen, "SQL Error", e.getMessage());
+        		}
+        		
+        	}
+        	else {
+        		MessageBox.showMessageBox(guiScreen, "Action", "Selected " + label);
+        	}
         }
+	}
+	
+	public class StoreInfoWindow extends MicrocenterWindow {
+		public StoreInfoWindow(final GUIScreen guiScreen, int storeId)
+		{
+			super(guiScreen, "Store Info", true);
+			
+			addComponent(new Button(""));
+		}
 	}
 }
